abfff2e5eeb3629f1e69e281d21c788cc3ce5fb2
Ruckard Smith <ruckard.smith@gmail.com>
2025-07-11 23:19:05 +0200

    Removed CONTRACTS from override_config.py

--- main.py
+++ main.py
@@ -210,10 +210,8 @@ from config import API_KEY, API_SECRET, BASE_URL, DB_PATH
 # === Default Values ===
 DEFAULT_SYMBOL_CONFIGS = {
     "BTC-PERP": {
-        "TRAILING_START": 0.4,
-        "TRAILING_STEP": 0.15,
+        "TRAILING_STEP_MULTIPLIER": 0.375, # Step is going to be 37.5% of calculated trailing start
         "TRAILING_COUNT": 2,
-        "CONTRACTS": 0.00001 / 0.00001 # Currency_number / Contract_size
     }
 }
 
--- override_config.py
+++ override_config.py
@@ -10,16 +10,12 @@
 
 SYMBOL_CONFIGS = {
     "BTC-PERP": {
-        #"TRAILING_START": 0.4,
         "TRAILING_STEP_MULTIPLIER": 0.375, # Step is going to be 37.5% of calculated trailing start
         "TRAILING_COUNT": 2,
-        "CONTRACTS": 0.00001 / 0.00001 # Currency_number / Contract_size
     },
     "ETH-PERP": {
-        #"TRAILING_START": 0.6,
         "TRAILING_STEP_MULTIPLIER": 0.33, # Step is going to be 33.0% of calculated trailing start
         "TRAILING_COUNT": 2,
-        "CONTRACTS": 0.0001 / 0.0001 # Currency_number / Contract_size
     }
 }
 
