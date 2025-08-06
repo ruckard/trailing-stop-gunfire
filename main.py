65dfedadff1aa2cec8c7c3fbf4c620c8c6c3c4ef
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-06 16:27:09 +0200

    Trend or range. (7)

--- main.py
+++ main.py
@@ -1877,7 +1877,7 @@ def place_range_positions(symbol, sides=("LONG", "SHORT"), lookback=50,
 
         # You may need to customize this to your real API structure:
         place_range_order(symbol=symbol,
-                          side=order_side,
+                          position_side=order_side,
                           contracts=contracts,
                           entry_price=entry_price,
                           take_profit=take_profit,
