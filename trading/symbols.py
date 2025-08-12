d494ce71d5b2d7228bf3743bd7b18f349003f4e7
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-12 23:25:37 +0200

    Trading refactor. (32)

--- trading/symbols.py
+++ trading/symbols.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from exchange.btse import fetch_top_symbols_by_volume
+from exchange.btse import fetch_top_symbols_by_volume, update_symbol_settings
 
 import sqlite3
 
