d40e1acac3fae0ca0b4368e9cd82c10fc91c1cca
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-21 13:00:03 +0200

    Public cache refactor. (14)

--- client/msg_utils.py
+++ client/msg_utils.py
@@ -11,18 +11,19 @@ def send_msg(sock, obj):
     sock.sendall(length + data)
 
 def recv_msg(sock):
-    # TODO: Fix this circular import in a different way
-    from utils import print_with_date, debug
     """Receive a pickled object with length prefix."""
+    from utils import print_with_date
     # Read message length (4 bytes)
     raw_len = recvall(sock, 4)
-    if not raw_len:
-        print_with_date("[DEBUG] No length prefix received")
+    if raw_len is None or len(raw_len) != 4:
+        print_with_date(f"[DEBUG] Invalid length prefix received: {raw_len}")
         return None
     msg_len = struct.unpack("!I", raw_len)[0]
     print_with_date(f"[DEBUG] Expecting {msg_len} bytes")
-    # Read the message data
     data = recvall(sock, msg_len)
+    if data is None:
+        print_with_date(f"[DEBUG] Incomplete data received, expected {msg_len} bytes")
+        return None
     print_with_date(f"[DEBUG] Received {len(data)} bytes")
     return pickle.loads(data)
 
