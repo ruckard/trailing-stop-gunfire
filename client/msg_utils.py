3ea35dc58d1c1a6668d1870628303e0c4f0fd812
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-21 12:37:08 +0200

    DEBUG (Temporary) msg_utils

--- client/msg_utils.py
+++ client/msg_utils.py
@@ -1,10 +1,12 @@
 import pickle
 import struct
+from utils import print_with_date, debug
 
 def send_msg(sock, obj):
     """Send a pickled object with length prefix."""
     data = pickle.dumps(obj)
     length = struct.pack("!I", len(data))  # 4-byte unsigned int, network order
+    print_with_date(f"[DEBUG] Sending {len(data)} bytes")
     sock.sendall(length + data)
 
 def recv_msg(sock):
@@ -12,10 +14,13 @@ def recv_msg(sock):
     # Read message length (4 bytes)
     raw_len = recvall(sock, 4)
     if not raw_len:
+        print_with_date("[DEBUG] No length prefix received")
         return None
     msg_len = struct.unpack("!I", raw_len)[0]
+    print_with_date(f"[DEBUG] Expecting {msg_len} bytes")
     # Read the message data
     data = recvall(sock, msg_len)
+    print_with_date(f"[DEBUG] Received {len(data)} bytes")
     return pickle.loads(data)
 
 def recvall(sock, n):
