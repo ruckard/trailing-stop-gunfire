a11ba6c384928f9c4a08c65b7efea043a405c21e
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-21 22:16:36 +0200

    Revert "DEBUG (Temporary) msg_utils (13)"
    This reverts commit f313cb67915d0850f7824c5a7578b8e21181aae8.

--- client/cache.py
+++ client/cache.py
@@ -7,13 +7,13 @@ PORT = 5005
 
 def api_cache_fetch(func_name, *args):
     """Ask daemon to run a cached public API call."""
-    print_with_date(f"DEBUG-api_cache_fetch: 1");
+    print_with_date(f"DEBUG-api_cache_fetch: symbol: {symbol} 1");
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print_with_date(f"DEBUG-api_cache_fetch: 2");
+        print_with_date(f"DEBUG-api_cache_fetch: symbol: {symbol} 2");
         s.connect((HOST, PORT))
-        print_with_date(f"DEBUG-api_cache_fetch: 3");
+        print_with_date(f"DEBUG-api_cache_fetch: symbol: {symbol} 3");
         s.sendall(pickle.dumps((func_name, args)))
-        print_with_date(f"DEBUG-api_cache_fetch: 4");
+        print_with_date(f"DEBUG-api_cache_fetch: symbol: {symbol} 4");
         data = pickle.loads(s.recv(4096))
-        print_with_date(f"DEBUG-api_cache_fetch: 5");
+        print_with_date(f"DEBUG-api_cache_fetch: symbol: {symbol} 5");
     return data
