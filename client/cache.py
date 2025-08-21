6b23262ba08561bc3fa408bc15d245528f6bf0cb
Ruckard Smith <ruckard.smith@gmail.com>
2025-08-21 22:19:17 +0200

    Public cache refactor. (21)

--- client/cache.py
+++ client/cache.py
@@ -1,6 +1,7 @@
 # client_cache.py
-import socket, pickle
+import socket
 from utils import print_with_date, debug
+from client.msg_utils import send_msg, recv_msg
 
 HOST = "127.0.0.1"
 PORT = 5005
@@ -9,6 +10,6 @@ def api_cache_fetch(func_name, *args):
     """Ask daemon to run a cached public API call."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(pickle.dumps((func_name, args)))
-        data = pickle.loads(s.recv(4096))
+        send_msg(s, (func_name, args))
+        data = recv_msg(s)
     return data
