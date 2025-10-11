ee2561ce617de20a52928acb939836b69555c5f0
Ruckard Smith <ruckard.smith@gmail.com>
2025-10-11 21:31:26 +0200

    Smooth how many candles a trade should be open. (4)

--- trading/trend.py
+++ trading/trend.py
@@ -4,7 +4,7 @@ from utils import print_with_date, debug
 import numpy as np
 import state
 import db.positions as positionsdb
-from trading.orders import get_dynamic_trade_max_candles
+from trading.common import get_dynamic_trade_max_candles
 
 TRENDRANGE_CACHE = {}
 
