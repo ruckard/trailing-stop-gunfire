73ef1f3ff54ecb085be5958e8f82bf2076557dbf
Ruckard Smith <ruckard.smith@gmail.com>
2025-11-16 10:05:04 +0100

    Removed dead symbols based on 1-minute candles. (2)

--- trading/trend.py
+++ trading/trend.py
@@ -228,7 +228,7 @@ def calculate_easy_trend10_with_rsi(symbol, lookback=50, rsi_period=14,
     """
 
     df_1minute = exchange.fetch_1m_ohlcv(symbol)
-    if is_dead_chat(df_1minute):
+    if is_dead_chart(df_1minute):
         debug(f"[{symbol}] was dismissed. 1-minute chart seems dead.")
         return 0.0
 
