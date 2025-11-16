bfa5dba33c8b696959f043a7313908dae8506ff5
Ruckard Smith <ruckard.smith@gmail.com>
2025-11-16 21:12:00 +0100

    Debug (Temporary) regarding is_dead_chart.

--- trading/trend.py
+++ trading/trend.py
@@ -272,7 +272,7 @@ def calculate_easy_trend10_with_rsi(symbol, lookback=50, rsi_period=14,
 
     df_1minute = exchange.fetch_1m_ohlcv(symbol)
     if is_dead_chart(df_1minute):
-        debug(f"[{symbol}] was dismissed. 1-minute chart seems dead.")
+        print_with_date(f"[{symbol}] was dismissed. 1-minute chart seems dead.")
         return 0.0
 
     if is_choppy_chart(df_1minute):
